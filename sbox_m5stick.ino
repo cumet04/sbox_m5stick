20dd43d61026d3dc5c6cd0e0e1885a17087e8228
inomoto <medalhkr@gmail.com>
2025-02-24 22:49:34 +0900

    wifiつながったっぽい

--- sbox_m5stick.ino
+++ sbox_m5stick.ino
@@ -1,13 +1,24 @@
+#include <WiFi.h>
 #include "M5StickCPlus.h"
+#include "secrets.h"
 
 void setup()
 {
+
     M5.begin();
-    Serial.begin(115200);
+    WiFi.begin(WIFI_SSID, WIFI_PASS);
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        M5.Lcd.print(".");
+    }
+    M5.Lcd.println("WiFi connected");
+    M5.Lcd.println("IP address: ");
+    M5.Lcd.println(WiFi.localIP());
+    // Serial.begin(115200);
 }
 
 void loop()
 {
-    Serial.println(ESP.getFreeHeap());
     delay(3000);
 }
